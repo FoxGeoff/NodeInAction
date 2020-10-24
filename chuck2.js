043d47751f1254ece8117f9acabecc0ed9dd271d
Geoff Fox <fox.geoffrey@gmail.com>
2020-10-24 15:50:39 -0700

    Message of the day

---
+++ chuck2.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+const fs = require("fs");
+
+let chuckCounter = 0;
+let chuckLine = 0;
+const chuckLines = 1064;
+
+if (fs.existsSync("chuckcounter.txt")) {
+  chuckLine = parseInt(fs.readFileSync("chuckcounter.txt"));
+  chuckLine++;
+  if (chuckLine > chuckLines) chuckLine = 0;
+}
+
+// A message of the day
+const rl = readline.createInterface({
+  input: fs.createReadStream("chuck.txt"),
+});
+
+rl.on("line", (line) => {
+  if (chuckCounter === chuckLine) {
+    fs.writeFileSync("chuckcounter.txt", chuckLine);
+    console.log(line);
+  }
+  chuckCounter++
+});
